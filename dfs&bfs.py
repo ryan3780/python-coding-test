302a7c6bee8de0dbcea59b73d127c3ecf7337939
ryan3780 <45477679+ryan3780@users.noreply.github.com>
2020-09-12 23:21:25 +0900

    Add files via upload

--- dfs&bfs.py
+++ dfs&bfs.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 # stack
 # 선입 후출 구조 또는 후입 선출 구조
 
@@ -13,11 +15,25 @@ stack.append(4)
 stack.pop()
 
 #최하단 원소부터 출력
-print(stack)
-#최상단 원소부터 출력
-print(stack[::-1])
-# que
+print('stack: ', stack)
 
+#최상단 원소부터 출력
+print('stack: ', stack[::-1])
+# que
+queue = deque()
+
+queue.append(5)
+queue.append(2)
+queue.append(3)
+queue.append(7)
+queue.popleft()
+queue.append(1)
+queue.append(4)
+queue.popleft()
+
+print(queue)
+queue.reverse()
+print(queue)
 # 재귀 함수
 
 # 탐색이란 많은 양의 데이터 중에서 원하는 데이터를 찾는 ㅘ정
